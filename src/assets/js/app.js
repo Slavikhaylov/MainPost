b24279061c6da925a758705f65ee5551baf1a59c
Slavikhaylov <hslavik627@gmail.com>
2022-02-07 18:02:04 +0500

    i dont know

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,10 @@
 console.log('file 1');
+
+
+const player = document.querySelector('.player'),
+      playBtn = document.querySelector('.play'),
+      audio = document.querySelector('.audio'),
+      progressContainer = document.querySelector('.progress__container'),
+      progress = document.querySelector('.progress'),
+      title = document.querySelector('.title'),
+      imgSrc = document.querySelector('.img__src'),
